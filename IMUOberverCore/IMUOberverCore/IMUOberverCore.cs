e271bfe01ae133e711973add46c83bedf0f815dc
nakano yosuke <naninuneno0411@gmail.com>
2019-02-03 23:55:28 +0900

    [dll] hello

---
+++ IMUOberverCore/IMUOberverCore/IMUOberverCore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMUOberverCore {
+    public static class IMUOberverCore {
+        public static string Hello { get { return "Hello IMUOberverCore."; } }
+    }
+}
