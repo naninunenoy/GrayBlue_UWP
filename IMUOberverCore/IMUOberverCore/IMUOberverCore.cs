250bc349550e432cecbcd68345f7fd09d013ce0d
nakano yosuke <naninuneno0411@gmail.com>
2019-02-04 01:32:05 +0900

    [dll] advertise test

--- IMUOberverCore/IMUOberverCore/IMUOberverCore.cs
+++ IMUOberverCore/IMUOberverCore/IMUOberverCore.cs
@@ -3,9 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Reactive.Disposables;
+using Windows.Devices.Bluetooth.Advertisement;
 
 namespace IMUOberverCore {
-    public static class IMUOberverCore {
-        public static string Hello { get { return "Hello IMUOberverCore."; } }
+    public class IMUOberverCore : IDisposable {
+        public string Hello { get { return "Hello IMUOberverCore."; } }
+
+        const int advertiseImtervalMS = 3000;
+        private BluetoothLEAdvertisementWatcher advertiseWatcher;
+        private Subject<BluetoothLEAdvertisement> advertiseSubject;
+
+        public IMUOberverCore() {
+            advertiseWatcher = new BluetoothLEAdvertisementWatcher();
+            advertiseSubject = new Subject<BluetoothLEAdvertisement>();
+            advertiseWatcher.SignalStrengthFilter.SamplingInterval = TimeSpan.FromMilliseconds(advertiseImtervalMS);
+            advertiseWatcher.Received += OnAdvertisementReceived;
+        }
+
+        public void Dispose() {
+            advertiseWatcher.Stop();
+            advertiseWatcher.Received -= OnAdvertisementReceived;
+            advertiseSubject.Dispose();
+        }
+
+        public IObservable<BluetoothLEAdvertisement> AdvertiseAsync() {
+            advertiseWatcher.Stop();
+            advertiseSubject.Dispose();
+            advertiseWatcher.Start();
+            return advertiseSubject.AsObservable();
+        }
+
+        private void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args) {
+            advertiseSubject.OnNext(args.Advertisement);
+        }
     }
 }
