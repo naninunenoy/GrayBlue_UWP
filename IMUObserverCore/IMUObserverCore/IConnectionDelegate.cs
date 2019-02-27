261094df6e03e193460080b37805ea569519f73c
nakano yosuke <naninuneno0411@gmail.com>
2019-02-27 23:53:41 +0900

    link shared

--- IMUObserverCore/IMUObserverCore/IConnectionDelegate.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace IMUObserverCore {
-    public interface IConnectionDelegate {
-        void OnConnectDone(string uuid);
-        void OnConnectFail(string uuid);
-        void OnConnectLost(string uuid);
-    }
-}
--- IMUObserverCore/IMUObserverCore/INotifyDelegate.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace IMUObserverCore {
-    public interface INotifyDelegate {
-        void OnButtonPush(string deviceId, string buttonName);
-        void OnButtonRelease(string deviceId, string buttonName, float pressTime);
-        void OnIMUDataUpdate(string deviceId, float[] acc, float[] gyro, float[] mag, float[] quat);
-    }
-}
