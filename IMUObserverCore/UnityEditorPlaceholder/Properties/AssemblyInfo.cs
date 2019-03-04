e1c6a4d25fb3a476a0e83e25377cf22faf6f0eae
nakano yosuke <naninuneno0411@gmail.com>
2019-03-05 02:05:34 +0900

    change Assembly name

--- IMUObserverCore/UnityEditorPlaceholder/Properties/AssemblyInfo.cs
+++ IMUObserverCore/UnityEditorPlaceholder/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("UnityEditorPlaceholder")]
+[assembly: AssemblyTitle("IMUObserverCore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnityEditorPlaceholder")]
+[assembly: AssemblyProduct("IMUObserverCore")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
